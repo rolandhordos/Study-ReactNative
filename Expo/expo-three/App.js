88bd72fc7f0d74dd77a503bb79d4e3518c678019
Roland Hordos <rolandhordos@me.com>
2017-10-14 23:34:28 -0700

    Addition of lower half of screen.  StyleSheet refactoring to reuse common layout.

--- Expo/expo-three/App.js
+++ Expo/expo-three/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styling.container}><Text>Hello World</Text></View>
+      <View style={styles.launchScreen}>
+        <View style={styles.topHalfContainer}>
+          <Text>Hello World</Text>
+        </View>
+        <ScrollView style={styles.bottomHalfContainer}>
+        </ScrollView>
+      </View>
     );
   }
 }
 
-const styling = StyleSheet.create({
-    container: {
-      flexDirection: 'column',  // layout vertically from the top
-      flex: 0.5,                // use only 50% of the screen
-      justifyContent: 'center', // vertical alignment
-      alignItems: 'center',     // horizontal alignment
-    }
+const halfScreenContainer = {
+  flexDirection: 'column',  // layout vertically from the top
+  flex: 1.0,                // use only 50% of the screen, which for 2 sibling views means each are 100%
+}
+
+let topHalfContainer = {
+  justifyContent: 'center', // vertical alignment
+  alignItems: 'center',     // horizontal alignment
+  backgroundColor: 'whitesmoke',
+}
+Object.assign(topHalfContainer, halfScreenContainer)  // merge in the half-screen properties
+
+const styles = StyleSheet.create({
+  launchScreen: {
+    flex: 1.0,
+  },
+  topHalfContainer: topHalfContainer,
+  bottomHalfContainer: halfScreenContainer,
 });
--- Expo/expo-three/App.test.js
+++ Expo/expo-three/App.test.js
@@ -18,25 +18,46 @@ describe('Launch Screen', () => {
 
   describe('Layout', () => {
 
-    expect(appViewStructure.type).toBe('View')
-
-    it('uses only the top half of the screen', () => {
-      // console.log(appViewStructure)
-      expect(appViewStructure.props.style.flexDirection).toBe('column')
-      expect(appViewStructure.props.style.flex).toBe(0.5)
+    it('has a simple top level view', () => {
+      expect(appViewStructure.type).toBe('View')
     })
 
-    describe('Text component', () => {
-      const textComponent = appViewStructure.children.find(entry => entry.type === "Text")
-      expect(textComponent).toBeDefined()
-      expect(textComponent.type).toBe('Text')
+    describe('Top Half - Greeting', () => {
+      const topHalf = appViewStructure.children[0]
+      expect(topHalf.type).toBe('View')
 
-      it('which says Hello World', () => {
-        const text = textComponent.children[0]
-        expect(text).toBe('Hello World')
+      it('uses only the top half of the screen', () => {
+        expect(topHalf.props.style.flexDirection).toBe('column')
+         // Two sibling components using the same
+        expect(topHalf.props.style.flex).toBe(1.0)
+      })
+
+      describe('Text component', () => {
+        const textComponent = topHalf.children.find(entry => entry.type === "Text")
+        expect(textComponent).toBeDefined()
+        expect(textComponent.type).toBe('Text')
+
+        it('says Hello World', () => {
+          const text = textComponent.children[0]
+          expect(text).toBe('Hello World')
+        })
       })
     })
-  })
 
+    describe('Bottom Half - Content', () => {
+      const bottomHalf = appViewStructure.children[1]
+
+      describe('Scrolling Boxes', () => {
+
+        it('uses a Scroll View', () => {
+          expect(bottomHalf.type).toContain('ScrollView')   // instance nameg has a prefix
+        })
+
+        it('fills the bottom half of the screen', () => {
+            expect(bottomHalf.props.style.flexDirection).toBe('column')
+            expect(bottomHalf.props.style.flex).toBe(1.0)
+        })
+      })
+    })
   })
 })
