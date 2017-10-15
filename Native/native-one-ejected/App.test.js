527ab7a6c89f0a6373aa7f88da7b00700f1044db
Roland Hordos <rolandhordos@me.com>
2017-10-15 15:01:38 -0700

    Starting with a working copy of expo-three at it's latest state of finishing release 0.0.4.

---
+++ Native/native-one-ejected/App.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import App from './App';
+
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+describe('Launch Screen', () => {
+  const appView = renderer.create(<App/>)
+  const appViewStructure = appView.toJSON()
+
+  it('Displays a known initial View', () => {
+    expect(appViewStructure).toMatchSnapshot()
+  })
+
+  describe('Layout', () => {
+
+    it('has a simple top level view', () => {
+      expect(appViewStructure.type).toBe('View')
+    })
+
+    describe('Top Half - Greeting', () => {
+      const topHalf = appViewStructure.children[0]
+      expect(topHalf.type).toBe('View')
+
+      it('uses only the top half of the screen', () => {
+        expect(topHalf.props.style.flexDirection).toBe('column')
+         // Two sibling components using the same
+        expect(topHalf.props.style.flex).toBe(1.0)
+      })
+
+      describe('Text component', () => {
+        const textComponent = topHalf.children.find(entry => entry.type === "Text")
+        expect(textComponent).toBeDefined()
+        expect(textComponent.type).toBe('Text')
+
+        it('says Hello World', () => {
+          const text = textComponent.children[0]
+          expect(text).toBe('Hello World')
+        })
+      })
+    })
+
+    describe('Bottom Half - Content', () => {
+      const bottomHalf = appViewStructure.children[1]
+
+      describe('Scrolling Boxes', () => {
+
+        it('uses a Scroll View', () => {
+          expect(bottomHalf.type).toContain('ScrollView')   // instance nameg has a prefix
+        })
+
+        it('fills the bottom half of the screen', () => {
+            expect(bottomHalf.props.style.flexDirection).toBe('column')
+            expect(bottomHalf.props.style.flex).toBe(1.0)
+        })
+
+        // ScrollView has an additional hidden "View" that does not appear in the JSX
+        const scrollViewInnerContainer = bottomHalf.children[0]
+        it('has a hidden container in the view hierarchy, before contained child views', () => {
+          expect(scrollViewInnerContainer).toBeDefined()
+          expect(scrollViewInnerContainer.type).toBe('View')
+        })
+
+        it('has five boxes arranged vertically within the scrolling area', () => {
+          const boxes = scrollViewInnerContainer.children
+          expect(boxes.length).toBe(5)
+        })
+      })
+    })
+  })
+})
