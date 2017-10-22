18e3b7b7339f46952426e929dbb9b47bd9ff1a5e
Roland Hordos <rolandhordos@me.com>
2017-10-21 17:20:07 -0700

    Expo and Jest testing a simple third party carousel component.
    Published it as a scoped NPM here: https://yarnpkg.com/en/package/@rolandhordos/react-native-carousel starting with version 0.11.0.
    
    Source here: https://github.com/rolandhordos/react-native-carousel

--- Expo/expo-four/App.js
+++ Expo/expo-four/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Carousel from '@rolandhordos/react-native-carousel'
 
-export default class App extends React.Component {
+export default class App extends Component {
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return(
+      <Carousel width={styles.slide.width} animate={false}>
+        <View style={styles.slide}>
+          <Text>Item 1</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text>Item 2</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text>Item 3</Text>
+        </View>
+      </Carousel>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  slide: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center',
+    width: 375,
+    backgroundColor: '#00CED1', // Dark Turquoise
+  }
+})
